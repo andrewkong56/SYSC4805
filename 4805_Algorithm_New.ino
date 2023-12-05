a458b95ba87e7b17df7c1a135d5cba52ca906235
Marc Laundy <marc.laundy@gmail.com>
2023-12-05 10:49:13 -0500

    Update 4805_Algorithm_New.ino

--- 4805_Algorithm_New.ino
+++ 4805_Algorithm_New.ino
@@ -20,9 +20,11 @@ int IRState;
 int IRFlag = 0;
 int DistLeft = 450;
 int DistRight = 450;
-const int DistMin = 15;
-const int DistStop = 9;
+//distance to avoid obstacle with ultrasonic
+const int DistMin = 30;
+const int DistStop = 20;
 int Turn = 0;
+int x;
 
 void setup() {
   Serial.begin(9600);
@@ -33,28 +35,39 @@ void setup() {
   pinMode(24, OUTPUT);
   pinMode(2, OUTPUT);
   pinMode(IRDetect, INPUT);
+  wdt_disable(); 
+  delay(3000);  
+  wdt_enable(WDTO_2S); 
 
 }
 
 void loop() {
+  wdt_reset(); 
   handle_ultrasonic();
 
-    if (analogRead(LFS_M) > 850 || analogRead(LFS_R) > 850 || analogRead(LFS_L) > 850) {
-      if (analogRead(LFS_L) > 950) {
-        motorRight();
-        delay(2000);
-      }
-      else if(analogRead(LFS_R) > 950) {
-        motorLeft();
-        delay(2000);
-      }
+  if (analogRead(LFS_M) > 850 || analogRead(LFS_R) > 850 || analogRead(LFS_L) > 850) {
+    if (analogRead(LFS_L) > 850) {
+      Serial.println(analogRead(LFS_L));
+      motorBack();
+      delay(800);
+      motorRight();
+      delay(2000);
+    }
+    else if(analogRead(LFS_R) > 850) {
+      Serial.println(analogRead(LFS_R));
+      motorBack();
+      delay(800);
+      motorLeft();
+      delay(2000);
+    }
 
-      else {
-        motorBack();
-        delay(800);
-        randomdirection();
-        delay(random(1000, 2000));
-      }
+    else {
+      Serial.println(analogRead(LFS_M));
+      motorBack();
+      delay(800);
+      randomdirection();
+      delay(random(1000, 2000));
+    }
     // input = counter%2;
     // switch(input) {
     //     case 0: {
@@ -142,10 +155,12 @@ void handle_ultrasonic() {
   if((Left > 0)) 
   {
     DistLeft = Left;
+    Serial.println(digitalRead(DistLeft));
   }
   if((Right > 0))
   {
     DistRight = Right; 
+    Serial.println(digitalRead(DistRight));
   }
   //Setting Turn to left or right
   if((DistRight < 25) || (DistLeft < 25) && (DistLeft != DistRight))
@@ -222,7 +237,7 @@ void handle_IRDetect() {
 }
 
 void randomdirection(){
-  int x = random(1,2);
+  x = random(1, 2);
   if (x = 1){
     motorRight();
   }
@@ -231,7 +246,6 @@ void randomdirection(){
   }
 
 }
-
 
 
 
